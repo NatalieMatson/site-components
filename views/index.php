644d9fd0c6c4369953720295c28a4d577f8b823e
NatalieMatson <nataliequinndesign@gmail.com>
2018-12-21 21:03:31 -0500

    Continued set-up. Importing Bootstrap and preparing test styles and text.

--- views/index.php
+++ views/index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE HTML>
-
 <html>
 <head>
 <title>Web Dev Testing Ground</title>
-<link rel="stylesheet" type="text/css" href="index.css">
+<link rel="stylesheet" type="text/css" href="./../styles/css/_custom.css">
+<link rel="stylesheet" type="text/css" href="./../styles/css/index.css">
 </head>
 
 <body>
-
+<?php echo "<h1>Test</h1>" ?>
 </body>
 </html>
