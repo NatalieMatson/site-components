dff42ed5a30350a72884aca7cd866e83e7c827b5
NatalieMatson <nataliequinndesign@gmail.com>
2018-12-24 13:15:18 -0500

    Started adding navigation bar.

--- views/index.php
+++ views/index.php
@@ -2,11 +2,39 @@
 <html>
 <head>
 <title>Web Dev Testing Ground</title>
-<link rel="stylesheet" type="text/css" href="./../styles/css/_custom.css">
-<link rel="stylesheet" type="text/css" href="./../styles/css/index.css">
+<link rel="stylesheet" type="text/css" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="../styles/css/_custom.css">
+<link rel="stylesheet" type="text/css" href="../styles/css/index.css">
+
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="../node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
 </head>
 
 <body>
-<?php echo "<h1>Test</h1>" ?>
+<div class="container-fluid">
+    <div class="row">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="container nav">
+                    <span class="logo">Web Dev<br />Testing Ground</span>
+                    <ul class="nav-items">
+                        <li><a href="#">Test</a></li>
+                        <li class="dropdown">Test
+                            <ul>
+                                <li class="dropdown-item">Test 2</li>
+                                <li class="dropdown-item">Test 2</li>
+                                <li class="dropdown-item">Test 2</li>
+                                <li class="dropdown-item">Test 2</li>
+                            </ul>
+                        </li>
+                        <li>Test</li>
+                        <li>Test</li>
+                        <li>Test</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 </body>
 </html>
